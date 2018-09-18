d1ce21fb43c742a3e7a2e8b394b9a746ffcf0506
Curtis Rutland <curtisrutland@gmail.com>
2018-09-18 16:07:01 -0500

    added resin files

--- upload.sh
+++ upload.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 scp start.sh package*.json crutland@192.168.29.24:~/ws/node/led-server/
-#scp src/server/*.js crutland@192.168.29.24:~/ws/node/led-server/
-#scp -r src/client crutland@192.168.29.24:~/ws/node/led-server/
 scp -r src/server src/client crutland@192.168.29.24:~/ws/node/led-server/
